22b399744111e5de97d2eb1afdd533c14c99fd29
AndrewAlyonkin <oxqq@ya.ru>
2021-07-14 17:48:16 +0800

    Initial commit

---
+++ src/main/java/edu/alenkin/busyman/BusyManBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.alenkin.busyman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyManBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusyManBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/alenkin/busyman/BusyManBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alenkin.busyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusyManBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
