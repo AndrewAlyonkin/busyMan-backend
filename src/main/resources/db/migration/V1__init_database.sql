e8c7dad2e9a6153ba7a2cfea473295f687ffdce4
AndrewAlyonkin <oxqq@ya.ru>
2021-07-14 20:54:50 +0800

    fix_schema

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -2,89 +2,79 @@
 SET global_timezone = '+0:00';
 SET NAMES utf8;
 
-DROP TABLE IF EXISTS 'category';
+DROP TABLE IF EXISTS category;
 
-DROP TABLE IF EXISTS 'priority';
+DROP TABLE IF EXISTS priority;
 
-DROP TABLE IF EXISTS 'stat';
+DROP TABLE IF EXISTS stat;
 
-DROP TABLE IF EXISTS 'task';
+DROP TABLE IF EXISTS task;
 
-DROP TABLE IF EXISTS 'users';
+DROP TABLE IF EXISTS users;
 
-DROP TABLE IF EXISTS 'roles';
+DROP TABLE IF EXISTS roles;
 
 CREATE TABLE users
 (
-    id               INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id               bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name             VARCHAR(40)                          NOT NULL,
     email            VARCHAR(40)                           NOT NULL,
     password         VARCHAR(40)                           NOT NULL,
     registered       TIMESTAMP           DEFAULT now() NOT NULL,
     enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+    completed_total   bigint DEFAULT 0,
+    uncompleted_total bigint DEFAULT 0
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE roles
 (
-    user_id INTEGER NOT NULL,
+    user_id bigint NOT NULL,
     role    VARCHAR(20),
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 
-
 CREATE TABLE 'category'
 (
-    'id'                bigint      NOT NULL AUTO_INCREMENT,
-    'user_id'                bigint      NOT NULL,
-    'title'             varchar(45) NOT NULL,
-    'completed_count'   bigint DEFAULT '0',
-    'uncompleted_count' bigint DEFAULT '0',
-    PRIMARY KEY (`id`),
+    id                bigint      NOT NULL AUTO_INCREMENT,
+    user_id                bigint      NOT NULL,
+    title             varchar(45) NOT NULL,
+    completed_count   bigint DEFAULT 0,
+    uncompleted_count bigint DEFAULT 0,
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    KEY 'index_title' ('title') INVISIBLE
+    KEY index_title (title) INVISIBLE
 );
 
-CREATE TABLE 'priority'
+CREATE TABLE priority
 (
-    'id'    bigint      NOT NULL AUTO_INCREMENT,
-    'title' varchar(45) NOT NULL,
-    'color' varchar(45) NOT NULL,
-    PRIMARY KEY ('id'),
-    KEY 'index_title' ('title')
+    id    bigint      NOT NULL AUTO_INCREMENT,
+    title varchar(45) NOT NULL,
+    color varchar(45) NOT NULL,
+    PRIMARY KEY (id),
+    KEY index_title (title)
 );
 
-CREATE TABLE 'stat'
+CREATE TABLE task
 (
-    `id`                bigint NOT NULL AUTO_INCREMENT,
-    `user_id`                bigint NOT NULL,
-    `completed_total`   bigint DEFAULT '0',
-    `uncompleted_total` bigint DEFAULT '0',
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE 'task'
-(
-    'id'          bigint                                                  NOT NULL AUTO_INCREMENT,
-    'user_id'          bigint                                                  NOT NULL,
-    'title'       varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    'completed'   int      DEFAULT '0',
-    'date'        datetime DEFAULT NULL,
-    'priority_id' bigint   DEFAULT NULL,
-    'category_id' bigint   DEFAULT NULL,
-    PRIMARY KEY ('id'),
+    id          bigint                                                  NOT NULL AUTO_INCREMENT,
+    user_id          bigint                                                  NOT NULL,
+    title       varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    completed   int      DEFAULT 0,
+    date        datetime DEFAULT NULL,
+    priority_id bigint   DEFAULT NULL,
+    category_id bigint   DEFAULT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    KEY 'fk_category_idx' ('category_id'),
-    KEY 'fk_priority_idx' ('priority_id'),
-    KEY 'index_title' ('title'),
-    KEY 'index_completed' ('completed'),
-    KEY 'index_date' ('date'),
-    CONSTRAINT 'fk_category' FOREIGN KEY ('category_id') REFERENCES 'category' ('id') ON DELETE SET NULL ON UPDATE RESTRICT,
-    CONSTRAINT 'fk_priority' FOREIGN KEY ('priority_id') REFERENCES 'priority' ('id') ON DELETE SET NULL ON UPDATE RESTRICT
+    KEY fk_category_idx (category_id),
+    KEY fk_priority_idx (priority_id),
+    KEY index_title (title),
+    KEY index_completed (completed),
+    KEY index_date (date),
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE SET NULL ON UPDATE RESTRICT,
+    CONSTRAINT fk_priority FOREIGN KEY (priority_id) REFERENCES priority (id) ON DELETE SET NULL ON UPDATE RESTRICT
 );
 
 
--- src/main/resources/db/migration/V2__triggers.sql
+++ src/main/resources/db/migration/V2__triggers.sql
@@ -6,11 +6,11 @@ CREATE DEFINER ='root'@'localhost' TRIGGER 'task_AFTER_INSERT'
 BEGIN
     -- если вставляем новую таску с категорией, и она завершена, увеличиваем счетчик завершенных задач для категории
     if (ifnull(NEW.category_id, 0) > 0 && ifnull(NEW.completed, 0) = 1) then
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) + 1) where id = NEW.category_id and user_id = NEW.user_id;
+        update category set completed_count = (ifnull(completed_count, 0) + 1) where id = NEW.category_id and user_id = NEW.user_id;
     end if;
     -- если вставляем новую таску с категорией, и она не завершена, увеличиваем счетчик незавершенных задач для категории
     if (ifnull(NEW.category_id, 0) > 0 && ifnull(NEW.completed, 0) = 0) then
-        update tasklist.category c
+        update category c
         set uncompleted_count = (ifnull(uncompleted_count, 0) + 1)
         where id = NEW.category_id  and user_id = NEW.user_id;
     end if;
@@ -18,10 +18,10 @@ BEGIN
     -- подсчет общей статистики по задачам
     if ifnull(NEW.completed, 0) = 1 then
         -- ...если завершена, то увеличиваем общий счетчик завершенных задач
-        update tasklist.stat set completed_total = (ifnull(completed_total, 0) + 1) where id = 1  and user_id = NEW.user_id;
+        update users set completed_total = (ifnull(completed_total, 0) + 1) where user_id = NEW.user_id;
     else
         -- ...а если не завершена - увеличиваем общий счетчик незавершенных задач
-        update tasklist.stat set uncompleted_total = (ifnull(uncompleted_total, 0) + 1) where id = 1  and user_id = NEW.user_id;
+        update users set uncompleted_total = (ifnull(uncompleted_total, 0) + 1) where user_id = NEW.user_id;
     end if;
 END;
 
@@ -33,20 +33,20 @@ CREATE DEFINER ='root'@'localhost' TRIGGER 'task_AFTER_DELETE'
 BEGIN
     -- если задача имеет категорию  и она завершена, уменьшаем счетчик завершенных задач для категории
     if (ifnull(old.category_id, 0) > 0 && ifnull(old.completed, 0) = 1) then
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id  and user_id = old.user_id;
+        update category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id  and user_id = old.user_id;
     end if;
     -- если задача имеет категорию, и она не завершена, уменьшаем счетчик невыполненных задач для категории
     if (ifnull(old.category_id, 0) > 0 && ifnull(old.completed, 0) = 0) then
-        update tasklist.category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id  and user_id = old.user_id;
+        update category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id  and user_id = old.user_id;
     end if;
 
     -- подсчет общей статистики по категориям
     if ifnull(old.completed, 0) = 1 then
         -- если завершена, то уменьшаем общий счетчик завершенных задач
-        update tasklist.stat set completed_total = (ifnull(completed_total, 0) - 1) where id = 1  and user_id = old.user_id;
+        update users set completed_total = (ifnull(completed_total, 0) - 1) where user_id = old.user_id;
     else
         -- если не завершена, то уменьшаем общий счетчик незавершенных задач
-        update tasklist.stat set uncompleted_total = (ifnull(uncompleted_total, 0) - 1) where id = 1  and user_id = old.user_id;
+        update users set uncompleted_total = (ifnull(uncompleted_total, 0) - 1) where user_id = old.user_id;
     end if;
 END;
 
@@ -61,76 +61,76 @@ BEGIN
     IF (ifnull(old.completed, 0) <> ifnull(new.completed, 0) && new.completed = 1 &&
         ifnull(old.category_id, 0) = ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик незавершенных задач на 1, увеличиваем счетчик завершенных задач на 1
-        update tasklist.category
+        update category
         set uncompleted_count = (ifnull(uncompleted_count, 0) - 1),
             completed_count   = (ifnull(completed_count, 0) + 1)
         where id = old.category_id  and user_id = old.user_id;
         -- уменьшаем общий счетчик незавершенных задач на 1, увеличиваем общий счетчик завершенных задач на 1
-        update tasklist.stat
+        update users
         set uncompleted_total = (ifnull(uncompleted_total, 0) - 1),
             completed_total   = (ifnull(completed_total, 0) + 1)
-        where id = 1  and user_id = old.user_id;
+        where user_id = old.user_id;
     END IF;
 
     -- статус задачи стал - не завершена и задача осталась в той же категории
     IF (ifnull(old.completed, 0) <> ifnull(new.completed, 0) && new.completed = 0 &&
         ifnull(old.category_id, 0) = ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик завершенных задач на 1, увеличиваем счетчик незавершенных задач на 1
-        update tasklist.category
+        update category
         set completed_count   = (ifnull(completed_count, 0) - 1),
             uncompleted_count = (ifnull(uncompleted_count, 0) + 1)
         where id = old.category_id  and user_id = old.user_id;
         -- уменьшаем общий счетчик завершенных задач на 1, увеличиваем общий счетчик незавершенных задач на 1
-        update tasklist.stat
+        update users
         set completed_total   = (ifnull(completed_total, 0) - 1),
             uncompleted_total = (ifnull(uncompleted_total, 0) + 1)
-        where id = 1  and user_id = old.user_id;
+        where  user_id = old.user_id;
     END IF;
 
     -- изменили категорию выполненной задачи
     IF (ifnull(old.completed, 0) = ifnull(new.completed, 0) && new.completed = 1 &&
         ifnull(old.category_id, 0) <> ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик завершенных задач в исходной категории
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id;
+        update category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id  and user_id = old.user_id;
         -- увеличиваем счетчик завершенных задач в новой категории
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) + 1) where id = new.category_id;
+        update category set completed_count = (ifnull(completed_count, 0) + 1) where id = new.category_id  and user_id = old.user_id;
     END IF;
 
     -- изменили категорию невыполненной задачи
     IF (ifnull(old.completed, 0) = ifnull(new.completed, 0) && new.completed = 0 &&
         ifnull(old.category_id, 0) <> ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик незавершенных задач в исходной категории
-        update tasklist.category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id;
+        update category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id and user_id = old.user_id;
         -- увеличиваем счетчик незавершенных задач в новой категории
-        update tasklist.category set uncompleted_count = (ifnull(uncompleted_count, 0) + 1) where id = new.category_id;
+        update category set uncompleted_count = (ifnull(uncompleted_count, 0) + 1) where id = new.category_id and user_id = old.user_id;
     END IF;
 
     -- изменили категорию и статус задачи с выполненной на невыполненную
     IF (ifnull(old.completed, 0) <> ifnull(new.completed, 0) && new.completed = 0 &&
         ifnull(old.category_id, 0) <> ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик завершенных задач в исходной категории
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id;
+        update category set completed_count = (ifnull(completed_count, 0) - 1) where id = old.category_id and user_id = old.user_id;
         -- увеличиваем счетчик незавершенных задач в новой категории
-        update tasklist.category set uncompleted_count = (ifnull(uncompleted_count, 0) + 1) where id = new.category_id;
+        update category set uncompleted_count = (ifnull(uncompleted_count, 0) + 1) where id = new.category_id and user_id = old.user_id;
         -- уменьшаем общий счетчик завершенных задач на 1, увеличиваем общий счетчик незавершенных задач на 1
-        update stat
+        update users
         set uncompleted_total = (ifnull(uncompleted_total, 0) + 1),
             completed_total   = (ifnull(completed_total, 0) - 1)
-        where id = 1;
+        where user_id = old.user_id;
     END IF;
 
     -- изменили категорию и статус задачи с невыполненной на выполненную
     IF (ifnull(old.completed, 0) <> ifnull(new.completed, 0) && new.completed = 1 &&
         ifnull(old.category_id, 0) <> ifnull(new.category_id, 0)) THEN
         -- уменьшаем счетчик незавершенных задач в исходной категории
-        update tasklist.category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id;
+        update category set uncompleted_count = (ifnull(uncompleted_count, 0) - 1) where id = old.category_id and user_id = old.user_id;
         -- увеличиваем счетчик завершенных задач в новой категории
-        update tasklist.category set completed_count = (ifnull(completed_count, 0) + 1) where id = new.category_id;
+        update category set completed_count = (ifnull(completed_count, 0) + 1) where id = new.category_id and user_id = old.user_id;
         -- уменьшаем общий счетчик незавершенных задач на 1, увеличиваем общий счетчик завершенных задач на 1
-        update tasklist.stat
+        update users
         set uncompleted_total = (ifnull(uncompleted_total, 0) - 1),
             completed_total   = (ifnull(completed_total, 0) + 1)
-        where id = 1;
+        where user_id = old.user_id;
     END IF;
 END;
 
