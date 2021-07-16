67a12cb485d21de5a4581cbf8a378584ce075c5a
AndrewAlyonkin <oxqq@ya.ru>
2021-07-16 19:28:49 +0800

    priorities_rest

--- src/main/java/edu/alenkin/busyman/rest/v1/PriorityRestController.java
+++ src/main/java/edu/alenkin/busyman/rest/v1/PriorityRestController.java
@@ -1,10 +1,18 @@
 package edu.alenkin.busyman.rest.v1;
 
+import edu.alenkin.busyman.model.Priority;
+import edu.alenkin.busyman.model.Priority;
+import edu.alenkin.busyman.security.SecurityUtils;
 import edu.alenkin.busyman.service.PriorityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static edu.alenkin.busyman.util.HttpUtils.buildResponse;
 
 /**
  * @author Alenkin Andrew
@@ -17,4 +25,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class PriorityRestController {
     static final String URL = "/api/v1/priorities";
     private final PriorityService service;
+
+    public ResponseEntity<List<Priority>> getAll() {
+        Integer userId = SecurityUtils.getAuthUserId();
+        log.debug("Get all priorities for user {}", userId);
+        return ResponseEntity.ok(service.getAll(userId));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Priority> get(@PathVariable Integer id) {
+        Integer userId = SecurityUtils.getAuthUserId();
+        log.debug("Get priority {} for user {}", id, userId);
+        return ResponseEntity.ok(service.get(id, userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Priority> create(@RequestBody Priority priority) {
+        Integer userId = SecurityUtils.getAuthUserId();
+        log.debug("Creating priority {} for user {}", priority, userId);
+        return buildResponse(priority, service.create(priority, userId));
+    }
+
+    @PutMapping
+    public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+        Integer userId = SecurityUtils.getAuthUserId();
+        log.debug("Updating priority {} for user {}", priority, userId);
+        return buildResponse(priority, service.create(priority, userId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Integer userId = SecurityUtils.getAuthUserId();
+        log.debug("Deleting priority {} for user {}", id, userId);
+        service.delete(id, userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
